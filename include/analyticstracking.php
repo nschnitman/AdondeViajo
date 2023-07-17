b61a8b8effa71a82209f5f264ba2145256622fa5
Nico <nicosch95@gmail.com>
2023-07-17 17:51:18 +0300

    NS-43 Analytics Update

--- include/analyticstracking.php
+++ include/analyticstracking.php
@@ -3,11 +3,15 @@
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-<?
-if (isset($_SESSION['XID'])) {
-	echo "ga('create', 'UA-102695084-1', 'auto', { 'userId': '".$_SESSION['XID']."' });\n";
-} else {
-  echo "ga('create', 'UA-102695084-1', 'auto');\n";
-}?>
+
+  <?php
+  if (isset($_SESSION['XID'])) {
+    echo "ga('create', 'UA-102695084-1', 'auto', { 'userId': '".$_SESSION['XID']."' });\n";
+  } else {
+    echo "ga('create', 'UA-102695084-1', 'auto');\n";
+  }
+  ?>
+
+  ga('set', 'transport', 'beacon');
   ga('send', 'pageview');
-</script> 	
+</script>
